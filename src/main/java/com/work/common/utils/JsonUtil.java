6c89bf0f629f79dc5fa5abdfcfa89b3ded38fa9d
haibo.wang <haibo.wang@yulore.com>
2017-07-12 10:29:35 +0800

    json字段禁止html转义

--- src/main/java/com/work/common/utils/JsonUtil.java
+++ src/main/java/com/work/common/utils/JsonUtil.java
@@ -1,9 +1,46 @@
 package com.work.common.utils;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 public class JsonUtil {
+	
+	/**
+	 * jsonobject禁止html转义，不支持多层jsonarray
+	 * @param json
+	 * @return
+	 */
+	public static JSONObject unescapeHtml(JSONObject json){
+		for(Object key:json.keySet()){
+			Object value = json.get(key);
+			if(value instanceof String){
+				value = StringEscapeUtils.unescapeHtml4(value.toString());
+			}else if(value instanceof JSONArray){
+				JSONArray value2 = (JSONArray) value;
+				if(value2.size()>0){
+					if(value2.get(0) instanceof String){
+						JSONArray value3 = new JSONArray();
+						for (Object object : value2) {
+							value3.add(StringEscapeUtils.unescapeHtml4(object.toString()));
+						}
+						value = value3;
+					}else if(value2.get(0) instanceof JSONObject){
+						JSONArray value3 = new JSONArray();
+						for (Object object : value2) {
+							value3.add(unescapeHtml((JSONObject)object));
+						}
+						value = value3;
+					}
+				}
+			}else if(value instanceof JSONObject){
+				value = unescapeHtml((JSONObject)value);
+			}
+			json.put(key, value);
+		}
+		return json;
+	}
 	
 	/**
 	 * 判断是否为数组，true表示是
